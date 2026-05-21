bc4a9063dc0a152c2246ee9b5a6771072bbba75b
Maddisetti Vennela <maddisettivennela184@gmail.com>
2026-05-21 22:22:27 +0530

    OZO-admins

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://localhost:3001/api'
-   apiUrl: "https://api.kanchira.com"
+   apiUrl: "https://ozo-admin.vercel.app"
+   
 };
